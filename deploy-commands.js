fc2c777839627ce057d19b7ad7747fa9cbc77f77
Mickael Lagarde <mlagarde.pro@gmail.com>
2026-06-15 17:03:11 +0200

    feat: add ping slash command

---
+++ deploy-commands.js
@@ -0,0 +1,21 @@
+import { REST, Routes } from 'discord.js';
+import { config } from 'dotenv';
+import * as commands from './commands/utility/index.js';
+
+config()
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+
+const body = Object.values(commands).map(cmd => cmd.data.toJSON());
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+    console.log('Enregistrement des commandes...');
+    await rest.put(
+        Routes.applicationCommands(CLIENT_ID),
+        { body }
+    );
+    console.log('Commandes enregistrées !');
+} catch (err) {
+    console.error(err);
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import { Client, GatewayIntentBits, Events, Collection } from 'discord.js';
 import { config } from 'dotenv';
+import * as commands from '../commands/utility/index.js';
 
 config();
+const TOKEN = process.env.BOT_TOKEN;
 
+//Bot loggin
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -12,11 +15,27 @@ const client = new Client({
     ]
 });
 
-
-const TOKEN = process.env.BOT_TOKEN;
-
 client.once(Events.ClientReady, (readyClient) =>{
     console.log(`Ready! logged as ${readyClient.user.tag}`)
+})
+
+//à check
+client.commands = new Collection();
+for (const command of Object.values(commands)) {
+    client.commands.set(command.data.name, command);
+}
+
+//interaction
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return
+
+    try {
+        await command.execute(interaction)
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 client.login(TOKEN);
